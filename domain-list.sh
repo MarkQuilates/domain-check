f69c6740214a2af01aacdab276a6d9ee240ed209
Mark Quilates <30779635+MarkQuilates@users.noreply.github.com>
2018-06-05 10:35:10 +0800

    Add files via upload

---
+++ domain-list.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat << EOF
+{ "data": [
+ { "{#DOMAIN}":"10bo88888.com" },
+ { "{#DOMAIN}":"vipuu5.com" },
+ { "{#DOMAIN}":"alpha88.com" },
+ { "{#DOMAIN}":"br821.com" },
+ { "{#DOMAIN}":"by77r.com" },
+ { "{#DOMAIN}":"max88lax.com" },
+ { "{#DOMAIN}":"mocha2.com" },
+ { "{#DOMAIN}":"nb88fa.com" }
+]}
+EOF
